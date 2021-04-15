16f762443e2891e4c2a2cf6200edec1239982939
Iftika <iftikarahmif@gmail.com>
2021-04-15 21:36:17 +0700

    lagi nyoba aja

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const path = require('path') //modul bawaan node js gak usah diinstall
+
+//Plugin Register for assets
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public/assets'),
+  prefix: '/public/assets', 
+  decorateReply: false,
+})
+//Plugin Register for forms
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public/forms'),
+  prefix: '/public/forms', 
+  decorateReply: false,
+})
+
+//Get HTML
+fastify.get('/', async (request, reply) => {
+  return reply.sendFile('public/index.html')
+})
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
