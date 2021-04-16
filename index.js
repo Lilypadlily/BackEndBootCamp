fe3ce59733de70f24924e56b0c9b7b26280ca2d8
Iftika <iftikarahmif@gmail.com>
2021-04-16 16:45:53 +0700

    Edit refraktori yang tidak diperlukan

--- index.js
+++ index.js
@@ -4,24 +4,16 @@ const fastify = require('fastify')({ logger: true })
 
 //Plugin Register for assets
 fastify.register(require('fastify-static'), require('./config/static').public)
-fastify.register(require('fastify-static'), require('./config/static').assets)
-fastify.register(require('fastify-static'), require('./config/static').forms)
+//fastify.register(require('fastify-static'), require('./config/static').assets)
+//fastify.register(require('fastify-static'), require('./config/static').forms)
+//refractory route
 fastify.register(require('./route/route'))
+//point of view buat data 
 fastify.register(require('point-of-view'),{
   engine:{
   handlebars: require('handlebars')
   }
 })
-
-//Get HTML
-//fastify.get('/', async (request, reply) => {
-  //reply.view('./templates/index.html',{ data : 'Iftika'}) // serving path.join(__dirname, 'public', 'myHtml.html') directly
-//})
-
-// Declare a route
-//fastify.get('/test', async (request, reply) => {
-  //return { Hallo: 'Segye' }
-//})
 
 // Run the server!
 const start = async () => {
